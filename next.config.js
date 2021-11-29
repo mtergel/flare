0581a65b6434a70840a65c2399a684a465618f6f
Tergel Munkhdelger <tergelm@gmail.com>
2021-11-29 15:54:50 +0800

    withBundleAnalyzer

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+});
+
 /** @type {import('next').NextConfig} */
-module.exports = {
+module.exports = withBundleAnalyzer({
   reactStrictMode: true,
   images: {
     domains: ["anyqfjvtgmdymcwdoeac.supabase.in"],
   },
-};
+});
