a1e78381c710a63d290b60044d22302c67610214
Tergel Munkhdelger <tergelm@gmail.com>
2021-12-04 19:48:28 +0800

    ssr uselocal storange

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -8,10 +8,14 @@ function useLocalStorage<T>(
   // Pass initial state function to useState so logic is only executed once
   const [storedValue, setStoredValue] = useState<T>(() => {
     try {
-      // Get from local storage by key
-      const item = window.localStorage.getItem(key);
-      // Parse stored json or if none return initialValue
-      return item ? JSON.parse(item) : initialValue;
+      if (typeof window !== "undefined") {
+        // Get from local storage by key
+        const item = window.localStorage.getItem(key);
+        // Parse stored json or if none return initialValue
+        return item ? JSON.parse(item) : initialValue;
+      } else {
+        return initialValue;
+      }
     } catch (error) {
       // If error also return initialValue
       console.log(error);
@@ -28,8 +32,10 @@ function useLocalStorage<T>(
         value instanceof Function ? value(storedValue) : value;
       // Save state
       setStoredValue(valueToStore);
-      // Save to local storage
-      window.localStorage.setItem(key, JSON.stringify(valueToStore));
+      if (typeof window !== "undefined") {
+        // Save to local storage
+        window.localStorage.setItem(key, JSON.stringify(valueToStore));
+      }
     } catch (error) {
       // A more advanced implementation would handle the error case
       console.log(error);
